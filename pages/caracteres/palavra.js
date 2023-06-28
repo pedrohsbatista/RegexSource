21e1ba28c899cb71961081e33f071671badd6016
Pedro Batista <pedrohenrique.sb07@gmail.com>
2023-06-28 19:46:33 -0300

    Procurar palavras

---
+++ pages/caracteres/palavra.js
@@ -0,0 +1,29 @@
+const text = "Casa A1, casa b2 e CASA C3";
+
+var results = [];
+
+match(/casa/);
+
+match(/casa/g);
+
+match(/casa/i);
+
+match(/casa/gi);
+
+match(/casa A1/gi);
+
+document.addEventListener("DOMContentLoaded", function() {    
+    let element = document.getElementById("text");
+    element.innerHTML = text;
+    element = document.getElementById("results");
+    results.forEach(function (result) {        
+        element.innerHTML += `<p>Expressão: ${result.pattern} - Match: ${result.match ?? "sem combinação"}</p>`;
+    })   
+});
+
+function match(pattern)
+{   
+    let matches = text.match(pattern);  
+    let match = matches ? matches.map(x => `"${x}"`).join(", ") : undefined;   
+    results.push({ pattern, match });
+}
