4151b118752a14a34c1d1485ed74bc2dab41058b
Pedro Batista <pedrohenrique.sb07@gmail.com>
2023-07-19 21:25:28 -0300

    Desafio - Lista de Arquivos

---
+++ pages/caracteres/desafioListaArquivo.js
@@ -0,0 +1,21 @@
+const text = "lista de arquivos mp3: jazz.mp3,rock.mp3,podcast.mp3,blues.mp3";
+
+var results = [];
+
+match(/\.mp3/g);
+
+document.addEventListener("DOMContentLoaded", function() {    
+    let element = document.getElementById("text");
+    element.innerHTML = text;
+    element = document.getElementById("results");
+    results.forEach(function (result) {        
+        element.innerHTML += `<p>Expressão: ${result.pattern} - Match: ${result.match ?? "sem combinação"}</p>`;
+    })   
+});
+
+function match(pattern)
+{   
+    let matches = text.match(pattern);  
+    let match = matches ? matches.map(x => `"${x}"`).join(", ") : undefined;   
+    results.push({ pattern, match });
+}
