05ed1d7f8b1708612b120552dda1ba149e9772a4
Pedro Batista <pedrohenrique.sb07@gmail.com>
2023-07-20 22:18:41 -0300

    Desafio - Três Espaços

---
+++ pages/caracteres/desafioTresEspacos.js
@@ -0,0 +1,23 @@
+const text = "a   b";
+
+var results = [];
+
+match(/a\s{3}b/g);
+
+match(/a\s+b/g);
+
+document.addEventListener("DOMContentLoaded", function() {    
+    let element = document.getElementById("text");
+    element.innerHTML = text;
+    element = document.getElementById("results");
+    results.forEach(function (result) {        
+        element.innerHTML += `<p>Expressão: ${result.pattern} - Match: ${result.match ?? "sem combinação"}</p>`;
+    })   
+});
+
+function match(pattern)
+{   
+    let matches = text.match(pattern);  
+    let match = matches ? matches.map(x => `"${x}"`).join(", ") : undefined;   
+    results.push({ pattern, match });
+}
