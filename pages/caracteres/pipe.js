761f2016ff245cc4f81a95df3dbeef9ec4c057c3
Pedro Batista <pedrohenrique.sb07@gmail.com>
2023-07-20 22:33:05 -0300

    Metacaracteres - Pipe

---
+++ pages/caracteres/pipe.js
@@ -0,0 +1,21 @@
+const text = "Você precisa responder sim, não ou não sei!";
+
+var results = [];
+
+match(/sim|não sei|não/g);
+
+document.addEventListener("DOMContentLoaded", function() {    
+    let element = document.getElementById("text");
+    element.innerHTML = text;
+    element = document.getElementById("results");
+    results.forEach(function (result) {        
+        element.innerHTML += `<p>Expressão: ${result.pattern} - Match: ${result.match ?? "sem combinação"}</p>`;
+    })   
+});
+
+function match(pattern)
+{   
+    let matches = text.match(pattern);  
+    let match = matches ? matches.map(x => `"${x}"`).join(", ") : undefined;   
+    results.push({ pattern, match });
+}
