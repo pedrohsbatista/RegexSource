f5afd427033b704bad0706bc1b15a4605650d531
Pedro Batista <pedrohenrique.sb07@gmail.com>
2023-07-21 23:16:00 -0300

    Unicode

---
+++ pages/caracteres/unicode.js
@@ -0,0 +1,21 @@
+const text = "a⌛b✌c👀";
+
+var results = [];
+
+match(/\u270C|\u231B|\u{1F440}/gu);
+
+document.addEventListener("DOMContentLoaded", function() {    
+    let element = document.getElementById("text");
+    element.innerHTML = text;
+    element = document.getElementById("results");
+    results.forEach(function (result) {        
+        element.innerHTML += `<p>Expressão: ${result.pattern} - Match: ${result.match ?? "sem combinação"}</p>`;
+    })   
+});
+
+function match(pattern)
+{   
+    let matches = text.match(pattern);  
+    let match = matches ? matches.map(x => `"${x}"`).join(", ") : undefined;   
+    results.push({ pattern, match });
+}
