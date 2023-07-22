77282ba07d9b131f7f329790ac1bc63eaac243b3
Pedro Batista <pedrohenrique.sb07@gmail.com>
2023-07-21 21:57:33 -0300

    Problema com uso de ponto como metacaracter

---
+++ pages/caracteres/problemaPonto.js
@@ -0,0 +1,27 @@
+const text = "Bom\ndia";
+
+//const text = "Bom\tdia";
+
+var results = [];
+
+match(/./gi);
+
+match(/.../gi);
+
+match(/..../gi);
+
+document.addEventListener("DOMContentLoaded", function() {    
+    let element = document.getElementById("text");
+    element.innerHTML = text;
+    element = document.getElementById("results");
+    results.forEach(function (result) {        
+        element.innerHTML += `<p>Expressão: ${result.pattern} - Match: ${result.match ?? "sem combinação"}</p>`;
+    })   
+});
+
+function match(pattern)
+{   
+    let matches = text.match(pattern);  
+    let match = matches ? matches.map(x => `"${x}"`).join(", ") : undefined;   
+    results.push({ pattern, match });
+}
