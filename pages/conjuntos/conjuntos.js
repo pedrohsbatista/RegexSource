59ac9244f0e0aeca03f87b6d6ea29a57d595fcc6
Pedro Batista <pedrohenrique.sb07@gmail.com>
2023-07-24 21:40:32 -0300

    Conjuntos

---
+++ pages/conjuntos/conjuntos.js
@@ -0,0 +1,23 @@
+const text = "Os números sorteados na quarta foram 1, 2, 3, 4, 5, 6 e não houve ganhadores";
+
+var results = [];
+
+match(/[02468]/g);
+
+match(/n[aã]./g);
+
+document.addEventListener("DOMContentLoaded", function() {    
+    let element = document.getElementById("text");
+    element.innerHTML = text;
+    element = document.getElementById("results");
+    results.forEach(function (result) {        
+        element.innerHTML += `<p>Expressão: ${result.pattern} - Match: ${result.match ?? "sem combinação"}</p>`;
+    })   
+});
+
+function match(pattern)
+{   
+    let matches = text.match(pattern);  
+    let match = matches ? matches.map(x => `"${x}"`).join(", ") : undefined;   
+    results.push({ pattern, match });
+}
