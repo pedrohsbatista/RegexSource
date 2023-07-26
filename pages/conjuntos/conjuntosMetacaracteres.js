ecf779ab220168b1bf2438a870e108517a599db2
Pedro Batista <pedrohenrique.sb07@gmail.com>
2023-07-25 21:25:23 -0300

    Conjuntos e metacaracteres

---
+++ pages/conjuntos/conjuntosMetacaracteres.js
@@ -0,0 +1,29 @@
+const text = "+.?*$-";
+
+var results = [];
+
+match(/[+.?*$-]/g);
+
+match(/[+.?*$-]./g);
+
+match(/[$-?]/g);
+
+match(/[$\-?]/g);
+
+match(/[-?]/g);
+
+document.addEventListener("DOMContentLoaded", function() {    
+    let element = document.getElementById("text");
+    element.innerHTML = text;
+    element = document.getElementById("results");
+    results.forEach(function (result) {        
+        element.innerHTML += `<p>Expressão: ${result.pattern} - Match: ${result.match ?? "sem combinação"}</p>`;
+    })   
+});
+
+function match(pattern)
+{   
+    let matches = text.match(pattern);  
+    let match = matches ? matches.map(x => `"${x}"`).join(", ") : undefined;   
+    results.push({ pattern, match });
+}
