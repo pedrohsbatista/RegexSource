e8086ffc733f29494413c2e689e55dc456f9d497
Pedro Batista <pedrohenrique.sb07@gmail.com>
2023-08-09 21:02:11 -0300

    Quantificadores - *

---
+++ pages/quantificadores/zeroOuMais.js
@@ -0,0 +1,21 @@
+const text = "A palavra é uma palavraa poderosa, pois uma única palavr pode mudar tudo...";
+
+var results = [];
+
+match(/palavra*/gi);
+
+document.addEventListener("DOMContentLoaded", function() {    
+    let element = document.getElementById("text");
+    element.innerHTML = text;
+    element = document.getElementById("results");
+    results.forEach(function (result) {        
+        element.innerHTML += `<p>Expressão: ${result.pattern} - Match: ${result.match ?? "sem combinação"}</p>`;
+    })   
+});
+
+function match(pattern)
+{   
+    let matches = text.match(pattern);  
+    let match = matches ? matches.map(x => `"${x}"`).join(", ") : undefined;   
+    results.push({ pattern, match });
+}
