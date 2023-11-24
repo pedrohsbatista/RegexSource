e1a7cafe9968e8084ff76d054b8ed00c7c5b46d3
Pedro Batista <pedrohenrique.sb07@gmail.com>
2023-11-24 20:13:43 -0300

    Grupos - Negative lookahead

---
+++ pages/grupos/negativeLookahead.js
@@ -0,0 +1,25 @@
+const text = `João é calmo, mas no trânsito fica nervoso.`;
+
+var results = [];
+
+match(/[\wÀ-ú]+/gi);
+
+match(/[\wÀ-ú]+\b(?!,)/gi);
+
+match(/[\wÀ-ú]+[\s|\.](?!,)/gi);
+
+document.addEventListener("DOMContentLoaded", function() {    
+    let element = document.getElementById("text");
+    element.innerHTML = text;
+    element = document.getElementById("results");
+    results.forEach(function (result) {             
+        element.innerHTML += `<p>Expressão: ${result.pattern} - Match: ${result.match ?? "sem combinação"}</p>`;
+    })   
+});
+
+function match(pattern)
+{   
+    let matches = text.match(pattern);  
+    let match = matches ? matches.map(x => `"${x}"`).join(", ") : undefined;   
+    results.push({ pattern, match });
+}
