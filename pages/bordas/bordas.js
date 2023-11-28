64763c8f5b062e26a11a7c62fdbb22e3255ee228
Pedro Batista <pedrohenrique.sb07@gmail.com>
2023-11-27 21:00:02 -0300

    Bordas - início e fim

---
+++ pages/bordas/bordas.js
@@ -0,0 +1,27 @@
+const text = `Romário era um ótimo jogador, mas agora é um político questionador`;
+
+var results = [];
+
+match(/r/gi);
+
+match(/^r/gi);
+
+match(/r$/gi);
+
+match(/^r.*r$/gi);
+
+document.addEventListener("DOMContentLoaded", function() {    
+    let element = document.getElementById("text");
+    element.innerHTML = text;
+    element = document.getElementById("results");
+    results.forEach(function (result) {             
+        element.innerHTML += `<p>Expressão: ${result.pattern} - Match: ${result.match ?? "sem combinação"}</p>`;
+    })   
+});
+
+function match(pattern)
+{   
+    let matches = text.match(pattern);  
+    let match = matches ? matches.map(x => `"${x}"`).join(", ") : undefined;   
+    results.push({ pattern, match });
+}
